3fada951b4c6068767c8e0349308927ef80a213b
wang <wanghanlinlin@126.com>
2023-08-21 21:35:29 +0800

    更改

--- DataStrucures/src/main/java/Link/index.java
+++ DataStrucures/src/main/java/Link/index.java
@@ -3,17 +3,17 @@ package Link;
 public class index {
     public static void main(String[] args) {
         NodeList nodeList = new NodeList();
-        Node<String> node = new Node<>(1+"",1);
+        Node<String> node = new Node<>(1 + "", 1);
         nodeList.add(node);
-        Node<String> node1 = new Node<>(2+"",2);
+        Node<String> node1 = new Node<>(2 + "", 2);
         nodeList.add(node1);
-        Node<String> node2 = new Node<>(5+"",5);
+        Node<String> node2 = new Node<>(5 + "", 5);
         nodeList.add(node2);
-        Node<String> node3 = new Node<>(4+"",4);
+        Node<String> node3 = new Node<>(4 + "", 4);
         nodeList.add(node3);
-        Node<String> node4 = new Node<>(7+"",7);
+        Node<String> node4 = new Node<>(7 + "", 7);
         nodeList.add(node4);
-        Node<String> node5 = new Node<>(110+"",2);
+        Node<String> node5 = new Node<>(110 + "", 2);
         nodeList.update(node5);
         nodeList.remove(7);
         System.out.println("-------------------");
@@ -28,7 +28,7 @@ public class index {
 
     }
 
-    public static void countNodeLength(NodeList nodeList){
+    public static void countNodeLength(NodeList nodeList) {
         Node<?> header = nodeList.getNode();
         int length = 0;
         while (header.next != null) {
@@ -38,18 +38,18 @@ public class index {
         System.out.println(length);
     }
 
-    public static Node getEndIndex(NodeList nodeList,int index){
+    public static Node getEndIndex(NodeList nodeList, int index) {
         //先遍历获取长度
         int nodeLength = getNodeLength(nodeList);
-        if(index>nodeLength){
+        if (index > nodeLength) {
             throw new RuntimeException("长度超出链表长度");
         }
         Node<?> header = nodeList.getNode();
-        index = nodeLength-index+1;
-        while (header.next!=null){
-            header= header.next;
+        index = nodeLength - index + 1;
+        while (header.next != null) {
+            header = header.next;
             index--;
-            if(index==0){
+            if (index == 0) {
                 break;
             }
         }
@@ -60,27 +60,30 @@ public class index {
     private static int getNodeLength(NodeList nodeList) {
         Node<?> header = nodeList.getNode();
         int length = 0;
-        while(header.next!=null){
+        while (header.next != null) {
             length++;
             header = header.next;
         }
         return length;
     }
 
-    private static void reverseNode(NodeList nodeList){
+    private static void reverseNode(NodeList nodeList) {
         Node<?> header = nodeList.getNode();
-        if(header.next==null||header.next.next==null){
+        if (header.next == null || header.next.next == null) {
             return;
         }
+        //当前的节点
         Node<?> cur = header.next;
+        //下一个节点
         Node<?> next = null;
-        Node<?> newHeader = new Node<>(null,-999);
-        while (cur!=null){
-            next=cur.next;
-            cur.next=newHeader.next;
-            cur=next;
+        //反转接受
+        Node<?> reverse = new Node<>(null, -999);
+        while (cur != null) {
+            next = cur.next;
+            cur.next = reverse.next;
+            reverse.next = cur;
+            cur = next;
         }
-        header.next=newHeader.next;
-        nodeList.setNode(header);
+        header.next = reverse.next;
     }
 }
